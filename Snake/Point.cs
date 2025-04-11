666fbe62fb8d1d8f62e5557ca3ab1122dc1bea2f
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-11 15:34:20 +0300

    Lesson Nr 3

---
+++ Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;//Public - Доступно из других классов
+        public int y;
+        public char sym;
+
+        public void Draw()//Draw - Просто название
+        {
+            Console.SetCursorPosition(x, y);//Устанавливает курсов по x & y
+            Console.Write(sym);//Печетает символ в месте курсора
+        }
+    }
+}
--- Snake/Program.cs
+++ Snake/Program.cs
@@ -4,30 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Snake
+namespace Snake//Название проекта
 {
-    class Program
+    class Program//Название класса
     {
-        static void Main( string[] args)
+        static void Main( string[] args)// void - ничего не возвращиает (нету return)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = 'x';
+            Point p1 = new Point();//Создание структуры с помощью вызова пустого конструктора
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();//Вызавает класс Point функцию Draw
 
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point();//Создание структуры с помощью вызова пустого конструктора
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();//Вызавает класс Point функцию Draw
             Console.ReadLine();
-        }
-        
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }

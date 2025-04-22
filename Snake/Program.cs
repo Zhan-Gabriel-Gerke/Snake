5150e2ffc159ad74203c8eaf11a897612483940f
Zhan-Gabriel Gerke <zangerke3@gmail.com>
2025-04-22 18:36:00 +0300

    Lesson Nr 13

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,45 +11,30 @@ namespace Snake//Название проекта
     {
         static void Main( string[] args)// void - ничего не возвращиает (нету return)
         {
-            Console.SetWindowSize(80, 25);
-
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            Draw(v1);
 
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake) fSnake;
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
-            
-            while (true)
-            { 
-                if (snake.Eat(food))
+            HorizontalLine h1 = new HorizontalLine(0, 5, 6, '&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach(var f in figures)
                 {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
+                    f.Draw();
                 }
-                else
-                {
-                    snake.Move();
-                }
-                Thread.Sleep(100); 
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
-                Thread.Sleep(100);
-                snake.Move();
-            }
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
         }
     }
 } 
